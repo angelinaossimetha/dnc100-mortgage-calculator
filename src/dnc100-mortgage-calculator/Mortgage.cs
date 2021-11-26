64615b9d1128b71936099a4c7d8bf431af0d3607
aossimetha <angelina@cccareers.org>
2021-11-26 12:37:43 -0500

    Add completed project w/o being able to run it

--- src/dnc100-mortgage-calculator/Mortgage.cs
+++ src/dnc100-mortgage-calculator/Mortgage.cs
@@ -6,34 +6,47 @@ namespace dnc100_mortgage_calculator
 {
     public class Mortgage
     {
+        public double principle; 
+        public double interest;
+        public int term; 
+        public int period; 
+
         public Mortgage(double principle, double interest, int term, int period)
         {
-
+            this.principle = principle; 
+            this.interest =  interest;
+            this.term = term;
+            this.period = period; 
         }
 
         public double Calculate()
         {
-            return 0;
+            double monthlyInterestRate = this.MonthlyInterestRate(this.interest, this.period);
+            int numberOfPayments = this.NumberOfPayments(this.term, this.period);
+            double compoundedInterestRate = this.CompoundedInterestRate(monthlyInterestRate, numberOfPayments);
+            double interestQuotient = this.InterestQuotient(monthlyInterestRate, compoundedInterestRate, numberOfPayments);
+            double monthlyPayment = this.principal * interestQuotient
+            return System.Math.Round(monthlyPayment, 2);
         }
 
         public double MonthlyInterestRate(double interest, int period)
         {
-            return 0;
+            return (interest / 100) / period;
         }
 
         public int NumberOfPayments(int term, int period)
         {
-            return 0;
+            return term * period;
         }
 
         public double CompoundedInterestRate(double monthlyInterestRate, int numberOfPayments)
         {
-            return 0;
+           return Math.Pow((1 + monthlyInterestRate), numberOfPayments);
         }
 
         public double InterestQuotient(double monthlyInterestRate, double compoundedInterestRate, int numberOfPayments)
         {
-            return 0;
+            return (monthlyInterestRate * compoundedInterestRate) / ((Math.Pow((1 + monthlyInterestRate), numberOfPayments)) - 1);
         }
     }
 }
--- src/dnc100-mortgage-calculator/Program.cs
+++ src/dnc100-mortgage-calculator/Program.cs
@@ -10,14 +10,27 @@ namespace dnc100_mortgage_calculator
             double principal, interestRate, monthlyPayment;
             int term, period;
             // Use .WriteLine to greet the user
+            Console.WriteLine("Welcome to the Mortgage Calculator App");
 
             // Use a mix of WriteLine and ReadLine to obtain the mortgage attributes (Making sure to typecast)
+            Console.WriteLine("Enter principal:");
+            principal = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter interest rate:");
+            interestRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter term:");
+            term = Int64.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter period:");
+            period = Int64.Parse(Console.ReadLine());
 
             // Create a new Mortgage with the given attributes;
-
+            mortgage = new Mortgage(principle, interest, term, period);
             // Use the Mortgage functions to calculate the monthly payment
-
+            monthlyPayment = mortgage.Calculate();
             // Use WriteLine to output the monthly payment
+            Console.WriteLine($"Monthly Payment: {monthlyPayment}");
 
         }
     }
